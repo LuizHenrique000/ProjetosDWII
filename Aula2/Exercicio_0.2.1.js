a77cee6c043fc710f05fa6548ebe8fc1886d74b4
LuizHenrique000 <luizlhra24@gmail.com>
2022-04-29 21:29:15 -0300

    Conclui exercicio 0.2.1

---
+++ Aula2/Exercicio_0.2.1.js
@@ -0,0 +1,9 @@
+const locadora = {
+  filmes: [
+    { nome: "Misterio na colonia", genero: "terror", ano: 2003, id: 0 },
+    { nome: "A culpa é das estrelas", genero: "drama", ano: 2014, id: 1 },
+    { nome: "A cinco passos de você", genero: "drama", ano: 2018, id: 2 },
+  ],
+};
+
+console.log(locadora.filmes);
