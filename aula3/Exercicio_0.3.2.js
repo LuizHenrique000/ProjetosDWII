288e6ba30e88b621b15567d9cda67ed89e7e5191
LuizHenrique000 <luizlhra24@gmail.com>
2022-05-08 21:57:07 -0300

    Conclui Exercicio_0.3.2

--- aula3/Exercicio_0.3.2.js
+++ aula3/Exercicio_0.3.2.js
@@ -1,20 +1,39 @@
 const tarefas = [
-    {
-        titulo: 'Passear com o cachorro',
-        concluida: false,
-    },
-    {
-        titulo: 'Comprar leite',
-        concluida: false,
-    },
-    {
-        titulo: 'Lavar a louça',
-        concluida: true,
-    }
-]
+    { titulo: "Passear com o cachorro", concluida: false, dias: 10 },
+    { titulo: "Comprar leite", concluida: false, dias: 5 },
+    { titulo: "Lavar louça", concluida: true, dias: 60 },
+];
 
-function retornaTarefaNaoConcluida(concluida) {
-    tar = concluida;
-    return tar == true;
+function retornaTarefasConcluidas() {
+    tarefasConcluidas = [];
+    for (let i = 0; i < tarefas.length; i++) {
+        if (tarefas[i].concluida == true) {
+            tarefasConcluidas = tarefas[i].titulo;
+            console.log(tarefasConcluidas);
+        }
+    }
 }
-retornaTarefaNaoConcluida(concluida);
+
+function retornaTarefasNaoConcluidas() {
+    let contador = 0;
+    for (let i = 0; i < tarefas.length; i++) {
+        if (tarefas[i].concluida == false) {
+            contador++;
+        }
+    }
+    console.log(contador);
+}
+
+function verificaTarefasConcluidasAbaixoDe30Dias() {
+    let contador = 0;
+    for (let i = 0; i < tarefas.length; i++) {
+        if (tarefas[i].concluida == true && tarefas[i].dias <= 30) {
+            contador++;
+        }
+    }
+    console.log("Existem " + contador + " tarefas concluidas em 30 dias");
+}
+
+retornaTarefasConcluidas();
+retornaTarefasNaoConcluidas();
+verificaTarefasConcluidasAbaixoDe30Dias();
