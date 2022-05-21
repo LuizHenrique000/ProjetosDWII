899592d5590ef2b221d7e10553beeedaefe18184
LuizHenrique000 <luizlhra24@gmail.com>
2022-05-20 22:31:41 -0300

    conclui metade do exercicio

---
+++ aula4/Exercicio_0.4.1.js
@@ -0,0 +1,30 @@
+const alunos = [
+    {
+        nome: 'Mathias',
+        notas: [
+            { cadeira: 'DW2', nota: 10 },
+            { cadeira: 'BD2', nota: 7 },
+        ],
+    },
+    {
+        nome: 'Luiza',
+        notas: [
+            { cadeira: 'DW2', nota: 8 },
+            { cadeira: 'BD2', nota: 8 },
+        ],
+    },
+    {
+        nome: 'Pedro',
+        notas: [
+            { cadeira: 'DW2', nota: 10 },
+            { cadeira: 'BD2', nota: 10 },
+        ],
+    }
+]
+
+const mediaGeral = alunos.map(myFunction);
+function myFunction(alunos) {
+    const media = alunos.notas.reduce((acumulador, proximoValor) => { }, 0)
+    alunos.mediaGeral = media;
+}
+console.log(alunos)
